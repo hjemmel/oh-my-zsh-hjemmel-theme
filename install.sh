1e79a2db5b0a363b0237d43ee41f2ab4f31fa508
Hans Jakob Emmel <hans.emmel@gmail.com>
2015-04-04 19:30:40 -0300

    First Commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Creating symlink from here to your oh-my-zsh themes path..."
+
+ln -f hjemmel.zsh-theme ~/.oh-my-zsh/themes/hjemmel.zsh-theme
+
+echo "Done ! (the symlink is ~/.oh-my-zsh/themes/hjemmel.zsh-theme)"
